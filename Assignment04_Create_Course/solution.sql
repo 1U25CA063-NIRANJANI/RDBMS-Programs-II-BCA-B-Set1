40e5ae1994d7f0e031d98c98494df9a467fdf4e7
1U25CA063-NIRANJANI <niruma3108@gmail.com>
2026-07-17 11:20:18 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,4 +1,7 @@
--- Create Course table
+use collegeDBbca;
+CREATE TABLE course(courseID VARCHAR (10),courseName varchar (30),credits int (10) PRIMARY KEY,departmentID varchar(2));
+INSERT INTO course(courseID,courseName,credits,departmentID) values(1, "BCA" , 10,2)
+SELECT * FROM course;-- Create Course table
 
 -- Insert three records
 
