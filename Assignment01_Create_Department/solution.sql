3e8ac29a76481123434614fdc5cffe8158f46b62
1U25CA063-NIRANJANI <niruma3108@gmail.com>
2026-07-17 11:15:49 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,4 +1,13 @@
--- Write your SQL here
+-create database departmentDBbca;
+use departmentDBbca;
+CREATE TABLE Department
+(
+DepartmentID INT(5) PRIMARY KEY,
+DepartmentName VARCHAR(20),
+HOD VARCHAR(20)
+);
+desc Department;
+- Write your SQL here
 
 -- Create Database
 
