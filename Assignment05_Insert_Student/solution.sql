89f971cab8b1ae12c29d085e070e413105bdb0c7
1U25CA063-NIRANJANI <niruma3108@gmail.com>
2026-07-17 11:21:13 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,4 +1,9 @@
--- Insert
+use collegeDBbca;
+use student;
+INSERT INTO student(studentID,studentName,DOB,gender,departmentID) values (1001,"Arun",31-10-2008,"Male",101);
+INSERT INTO student(studentID,studentName,DOB,gender,departmentID) values (1002,"Divya",28-10-2008,"Female",102);
+INSERT INTO student(studentID,studentName,DOB,gender,departmentID) values (1003,"karthik",02-11-2008,"Male",101);
+SELECT * FROM student-- Insert
 
 -- 1001 Arun
 
