e9dec59627958b73e22608a6b5b0237d4626f160
1U25CA063-NIRANJANI <niruma3108@gmail.com>
2026-07-29 10:28:49 +0530

    Update solution.sql

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,5 +1,8 @@
-ALTER TABLE Student
-ADD Email VARCHAR(30),
-ADD PhoneNumber VARCHAR(10);
-
-DESC Student;
+use collegeDBbca;
+alter TABLE student
+add(
+	Email VARCHAR(10),
+    PhoneNumber int(10)
+);
+SELECT * FROM student
+DESC student
