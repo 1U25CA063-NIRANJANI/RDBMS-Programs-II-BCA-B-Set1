c5d85fc2478909a4898e6ada76bda697664f398e
1U25CA063-NIRANJANI <niruma3108@gmail.com>
2026-07-29 10:28:09 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,7 +1,12 @@
-CREATE TABLE Student (
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5)
+use collegeDBbca;
+CREATE TABLE sudent(
+	studentID int(5) PRIMARY KEY,
+    studentName VARCHAR (20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR (10) NOT NULL,
+    DepartmentID int(5) NOT NULL,
+    CONSTRAINT UQ_StudentName UNIQUE
+(StudentName)
 );
+SELECT * FROM student
+DESC student
